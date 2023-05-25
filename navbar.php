a458e678f96daefd4be54fcae8e03ede8889769a
shalsabillaput <shalsabilla.amanda.putri-2021@vokasi.unair.ac.id>
2023-05-25 12:36:17 +0700

    navbar

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav>
+    <div class='nav-logo'>
+        <a href="index.php">
+            <img src="images/logo-utama.png" width="160" height="25" alt="">
+        </a>
+    </div>
+    <div class='nav-menu-container'>
+        <a href="" class="">Aksi</a>
+        <a href="" class="">Jelajahi</a>
+        <a href="donasi.php" class="">Donasi</a>
+        <div class="nav-search-container">
+            <input type="text" class="nav-search-input" placeholder="">
+            <img src="images/search.png" class="nav-searcg-img" alt="">
+        </div>
+    </div>
+</nav>
