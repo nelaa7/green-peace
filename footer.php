dfbce4db40449ef83430820a0f9ad01c6b735793
nadya <nadndyaa@gmail.com>
2023-05-25 13:50:51 +0700

    up

--- footer.php
+++ footer.php
@@ -1,36 +1,41 @@
-<section>
-        <div class="news-container" style="font-family: Inter;">
-            <div class="news-header">Bersama kita bisa membuat perubahan.</div>
-            <div class="news-header" style="font-size: 20px;color:#636363;">Bergabung sekarang untuk mendapatkan informasi terbaru seputar kampanye lingkungan.</div>
-        </div>
-    </section>
-    <section>
-        <div style="background-color: #D4F1B8; font-family: Inter;">
-            <div class="news-container" style="padding-bottom: 40px; margin-bottom: 0px;">
-                <div class="grid-auto-column" style="padding-top: 30px;">
+<footer>
+    <div class="container-footer">
+        <p style="margin-left: 3%;">Indonesia</p>
+        <hr style="">
+        <div class="grid-auto-column" style="margin-top: 1em;">
+            <ul>
+                <li>Tentang Kami</li>
+                <li>Kontak</li>
+                <li>Bekerja di Greenpeace</li>
+                <li>Pers</li>
+                <li>Supporter Care</li>
+                <li>Peta Situs</li>
+            </ul>
+            <ul>
+                <li>Kebijakan Privasi</li>
+                <li>Hak Cipta</li>
+                <li>Ketentuan</li>
+                <li>Kebijakan Komunitas</li>
+                <li>FAQ</li>
+                <li>Arsip</li>
+            </ul>
+            <div>
+                <div>Follow Us</div>
+                <div class="" style="display: flex; gap: 30px;margin-top: 1rem;">
                     <div>
-                        <div >Email</div>
-                        <input class="nav-search-input" style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="email" name="" id="">
+                        <img src="images/facebook.png" alt="">
                     </div>
                     <div>
-                        <div >Nama Depan</div>
-                        <input class="nav-search-input" style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="nama_depan" name="" id="">
+                        <img src="images/twitter.png" alt="">
                     </div>
                     <div>
-                        <div >Nama Belakang</div>
-                        <input class="nav-search-input" style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="nama_belakang" name="" id="">
+                        <img src="images/youtube.png" alt="">
                     </div>
-                   
+                    <div>
+                        <img src="images/whatsapp.png" alt="">
+                    </div>
                 </div>
-                <div class="news-slug">
-                    Greenpeace Southeast Asia memerlukan informasi kontak yang Anda berikan kepada kami untuk menghubungi Anda tentang produk dan layanan kami. Anda dapat berhenti berlangganan dari komunikasi ini kapan pun. Untuk informasi tentang cara berhenti berlangganan,serta praktik privasi dan komitmen kami untuk melindungi privasi Anda, tinjau Kebijakan Privasi kami.
-                </div>
-                <div class="news-button" style="font-weight: bold;">BERGABUNG SEKARANG</div>
             </div>
         </div>
-    </section>
-    <?php include 'footer.php'; ?>
-
-</body>
-
-</html>
+    </div>
+</footer>
