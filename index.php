da50d63e145426585fab6f7378d28b67e3b78148
Berlian Qulintyana <Berlianq@users.noreply.github.com>
2023-05-25 12:43:26 +0700

    artikel

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <!-- hero section -->
+    <section>
+        <div class="hero-container">
+            <div class="hero-items-container">
+                <span class="" style="font-size: 28px; width: 40%;flex-basis: 1">Selamatkan Grime Nawa, Selamatkan Hutan
+                    Papua</span>
+                <span class="" style="font-size: 16px; width: 60%;flex-basis: 1;">Lebih dari 100 hektar hutan Lembah
+                    Grime
+                    Nawa
+                    sudah hilang dan akan terus bertambah.
+                    Sanksi tegas Para Pengusaha Nakal!
+                </span>
+                <a class="hero-items-button" href="artikel-grime-nawa.php">Pelajari Selengkapnya</a>
+            </div>
+        </div>
+    </section>
+    <!-- artikel terbaru -->
+    <section>
+        <div class="news-container">
+            <div class="news-header">Artikel Terbaru</div>
+            <div class="news-item">
+                <div>
+                    <img src="images/news1.png" style="width: 100%; height: 100%;object-fit: cover;" alt="">
+                </div>
+                <div>
+                    <div class="news-tag"><a href="">Cerita #iklim</a></div>
+                    <div class="news-tittle"><a href="">Lebaran di Desa yang Tenggelam</a></div>
+                    <div class="news-slug">Merayakan Idulfitri di desa yang terdampak krisis iklim.</div>
+                    <span class="news-author">Greenpeace Indonesia</span>
+                    <span class="news-date-released">23 April 2023</span>
+                </div>
+            </div>
+            <div class="news-item">
+                <div>
+                    <img src="images/news2.png" style="width: 100%; height: 100%;object-fit: cover;" alt="">
+                </div>
+                <div>
+                    <div class="news-tag"><a href="">Cerita #Ummah4Earth #Polusi #iklim #kota #aktivisme</a></div>
+                    <div class="news-tittle"><a href="">Siniar Seru Untuk Menemani Lebaranmu</a></div>
+                    <div class="news-slug">Ramadan selalu menjadi waktu yang tepat untuk melakukan introspeksi dan
+                        perbaikan diri.
+                        Kebaikan dan perbaikan yang sudah kita jalani selama Ramadan sepatutnya tetap dilanjutkan agar
+                        keberkahan Ramadan tidak berhenti begitu saja.</div>
+                    <span class="news-author">Rizka Aulia</span>
+                    <span class="news-date-released">19 April 2023</span>
+                </div>
+            </div>
