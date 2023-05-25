4c9b05723148801ce633eb72ace04b933711de37
nadya <nadndyaa@gmail.com>
2023-05-25 14:10:57 +0700

    updt

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer>
+    <div class="container-footer">
+        <p style="margin-left: 3%;">Indonesia</p>
+        <hr style="">
+        <div class="grid-auto-column" style="margin-top: 1em;">
+            <ul>
+                <li>Tentang Kami</li>
+                <li>Kontak</li>
+                <li>Bekerja di Greenpeace</li>
+                <li>Pers</li>
+                <li>Supporter Care</li>
+                <li>Peta Situs</li>
+            </ul>
+            <ul>
+                <li>Kebijakan Privasi</li>
+                <li>Hak Cipta</li>
+                <li>Ketentuan</li>
+                <li>Kebijakan Komunitas</li>
+                <li>FAQ</li>
+                <li>Arsip</li>
+            </ul>
+            <div>
+                <div>Follow Us</div>
+                <div class="" style="display: flex; gap: 30px;margin-top: 1rem;">
+                    <div>
+                        <img src="images/facebook.png" alt="">
+                    </div>
+                    <div>
+                        <img src="images/twitter.png" alt="">
+                    </div>
+                    <div>
+                        <img src="images/youtube.png" alt="">
+                    </div>
+                    <div>
+                        <img src="images/whatsapp.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -198,6 +198,8 @@
             </div>
         </div>
     </section>
+    
     <?php include 'footer.php'; ?>
+    </html>
 
     
