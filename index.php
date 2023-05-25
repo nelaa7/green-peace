8356759de8e79de0618036ff3ad066782b2a7931
nelaa7 <nelaanjani22@gmail.com>
2023-05-25 14:01:38 +0700

    error footer

--- index.php
+++ index.php
@@ -200,4 +200,41 @@
         </div>
     </section>
 
+    <section>
+        <div class="news-container" style="font-family: Inter;">
+            <div class="news-header">Bersama kita bisa membuat perubahan.</div>
+            <div class="news-header" style="font-size: 20px;color:#636363;">Bergabung sekarang untuk mendapatkan informasi terbaru seputar kampanye lingkungan.</div>
+        </div>
+    </section>
+    <section>
+        <div style="background-color: #D4F1B8; font-family: Inter;">
+            <div class="news-container" style="padding-bottom: 40px; margin-bottom: 0px;">
+                <div class="grid-auto-column" style="padding-top: 30px;">
+                    <div>
+                        <div >Email</div>
+                        <input class="nav-search-input" style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="email" name="" id="">
+                    </div>
+                    <div>
+                        <div >Nama Depan</div>
+                        <input class="nav-search-input" style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="nama_depan" name="" id="">
+                    </div>
+                    <div>
+                        <div >Nama Belakang</div>
+                        <input class="nav-search-input" style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="nama_belakang" name="" id="">
+                    </div>
+                   
+                </div>
+                <div class="news-slug">
+                    Greenpeace Southeast Asia memerlukan informasi kontak yang Anda berikan kepada kami untuk menghubungi Anda tentang produk dan layanan kami. Anda dapat berhenti berlangganan dari komunikasi ini kapan pun. Untuk informasi tentang cara berhenti berlangganan,serta praktik privasi dan komitmen kami untuk melindungi privasi Anda, tinjau Kebijakan Privasi kami.
+                </div>
+                <div class="news-button" style="font-weight: bold;">BERGABUNG SEKARANG</div>
+            </div>
+        </div>
+    </section>
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
+
     
