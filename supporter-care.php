b9a272855db4c13c55e4d70921a2352b9dc5ca8b
nadya <nadndyaa@gmail.com>
2023-06-08 08:45:35 +0700

    up tk-sc

---
+++ supporter-care.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+    <style>
+        h5 {
+            font-size: 20px;
+        }
+
+        .container-aksi-cegah-api .grid-auto-column:nth-child(even) {
+            background-color: #ECECEC;
+        }
+
+        .container-aksi-cegah-api .grid-auto-column:nth-child(odd) {
+            background-color: #F5F7F8;
+        }
+
+        .grid-auto-column {
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <!-- hero section -->
+
+    <div class="f-inter"
+        style="background-image: url(images/supporter-care.png);background-repeat: no-repeat;padding: 130px 130px;background-size: 100%;font-size: 20px;">
+        <h6 style="color: #006DFD;">#TentangKami</h6>
+
+        <h1 class="footer-title-header">Supporter Care</h1>
+        <h4 class="footer-title-subheader mb-4">Donor-donor kami merupakan penggerak penting
+            kerja Greenpeace dalam melindungi lingkungan.</h4>
+        <a class="hero-items-button mb-4" style="color: white;font-weight: bold;padding: 10px;">Kirim Email</a>
+
+    </div>
+    <div class="news-container f-inter ">
+        <div class="container-aksi-cegah-api">
+            <p>
+                Kami harap website ini dapat memberikan informasi memadai tentang kami, kampanye
+                kami dan apa yang kami lakukan. Jika Anda adalah donatur Greenpeace Indonesia
+                dan membutuhkan informasi lebih lanjut seputar donasi, tim Supporter Care akan dengan
+                senang hati membantu Anda.
+            </p>
+            <div class="mb-4 bold">Email: <span class="text-link">supporterservices.id@greenpeace.org</span></div>
+            <div class="bold mb-4">Nomer telepon inbound:</div>
+            <div class="px-3 mb-4">
+                <p>622139524323</p>
+                <p>622139524390</p>
+                <p>622139524391</p>
+            </div>
+            <div class="bold mb-4">
+                Nomer telepon outbound:
+            </div>
+            <div class="px-3 mb-4">
+                <p>622139524325</p>
+                <p>622139524326</p>
+                <p>622139524327</p>
+                <p>622139524392</p>
+                <p>622150928647</p>
+                <p>622128565314</p>
+            </div>
+            <div class="text-center bold" style="margin-bottom: 3em;">FAQ Donasi</div>
+            <div class="grid-auto-column">
+                <h5>1. Bagaimana Greenpeace Indonesia mendapatkan dana kampanye</h5>
+                <div>Kami mendapatkan dana dari individu hebat seperti Anda. Kami juga
+                    mendapatkan dana dari yayasan yang sudah kami periksa secara
+                    detil mengenai latar belakangnya. Kami hanya bisa melakukan
+                    kampanye lingkungan karena kekuatan masyarakat.</div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>2. Bagaimana cara berdonasi ke Greenpeace Indonesia?</h5>
+                <div>
+                    <p>
+                        Dukungan Anda adalah tulang punggung kampanye Greenpeace.
+                        Anda dapat berdonasi dengan cara berikut:
+                    </p>
+                    <p>
+                        1. Melalui laman donasi Greenpeace Indonesia
+                    </p>
+                    <p>
+                        2. Melalui yang berada di lokasi publik seperti di halte, stasiun, jembatan penyebrangan, atau
+                        pusat perbelanjaan.
+                    </p>
+                    <p>
+                        3. Melalui Telefundraisingkami yang menghubungi Anda melalui panggilan telepon.
+                    </p>
+                    <p>
+                        4. Melalui laman crowdfunding kami di Patungan Iklim dan Jaga Bumi
+                    </p>
+                    <p>
+                        Kami sangat menghargai dukungan yang Anda berikan.
+                    </p>
+
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>3. Apakah Greenpeace Indonesia menjual data saya?</h5>
+                <div>
+                    <p>Kami melindungi data Anda dan tidak akan menukarkannya dalam
+                        bentuk apapun. Kami menjamin kerahasiaan data Anda.</p>
+                    <p>
+                        Kami tunduk pada konstitusi atau aturan hukum yang berlaku untuk
+                        menjaga data demi mempertahankan reputasi kami. Dan memang
+                        berwajib karena itu telah menyalahi Undang-Undang yang berlaku.
+                        Indonesia.
+                    </p>
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>4. Apa pertanggungjawaban Greenpeace Indonesia dalam
+                    dananya?</h5>
+                <div>
+                    <p>
+                        Pekerjaan untuk melindungi lingkungan adalah perjuangan yang
+                        panjang. Oleh karenanya, kami memerlukan dana yang berkelanjutan
+                        Metode tersebut juga lebih aman dan transparan.
+                    </p>
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>6. Kalau saya ditelepon oleh Greenpeace Indonesia, apakah itu
+                    benar?</h5>
+                <div>
+                    <p>
+                        Betul. Kami memiliki bagian telefundraising yang nomornya
+                        tercantum pada
+                        dilatih secara profesional serta memiliki akuntabilitas yang tinggi. 
+                    </p>
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>7. Saya ingin berdonasi melalui transfer saja, apakah bisa?</h5>
+                <div>Anda bisa berdonasi melalui donasi web melalui link
+                    tetapi, kami tetap menghimbau untuk memberikan donasi per bulan
+                    demi kelangsungan kerja perlindungan lingkungan. Untuk
+                    Supporter Care kami di </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>8. Apakah saya bisa memberikan donasi melalui komunitas?</h5>
+                <div>
+                    <p>
+                        Tentu saja! Komunitas adalah elemen penting di masyarakat untuk
+                        supporterservices.id@greenpeace.or
+                    </p>
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>9. Apakah saya bisa meminta tanda terima donasi yang sudah saya
+                    berikan?</h5>
+                <div>
+                    <p>
+                        Kami berterima kasih atas dukungan yang Anda berikan untuk
+                        menjaga Bumi kita. Greenpeace akan memberikan tanda terima
+                        donasi 1 tahun (annual receipt) untuk donatur bulanan dan
+                        mengirimkannya ke alamat email donatur. Jika Anda memberikan
+                        donasi satu-kali, setelah donasi berhasil, donatur akan langsung
+                        menerima tanda terima elektronik melalui email. Untuk mengetahui
+                        status donasi Anda, silakan menghubungi
+                        supporterservices.id@greenpeace.org
+                    </p>
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>10. Donasi yang saya berikan melalui website Greenpeace
+                    dinyatakan gagal atau error. Apakah donasi saya tetap diproses?</h5>
+                <div>
+                    <p>
+                        Mohon maaf atas ketidaknyamanan yang Anda alami. Untuk
+                        mengetahui status donasi Anda, silakan menghubungi
+                        supporterservices.id@greenpeace.org
+                    </p>
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>11. Apakah saya dapat mengajukan pembatalan donasi (berhenti
+                    berdonasi)?</h5>
+                <div>
+                    <p>
+                        Tentu saja, bisa.
+                    </p>
+                    <p>Anda dapat mengajukan pembatalan donasi yang sedang berjalan
+                        ke Greenpeace Indonesia, dengan ketentuan:</p>
+                    <p>
+                        * Permintaan pembatalan (berhenti berdonasi) serta surat kuasa
+                        pendebetan Rekening / Kartu Kredit berlaku 1 (satu) bulan sejak
+                        * Kirim permintaan pembatalan atau berhenti berdonasi melalui
+                        * Beri kami informasi yang memadai tentang detail donasi dan data
+                        Anda.
+                    </p>
+                    <p>
+                        Silakan kirim surat pembatalan donasi ke:
+                        supporterservices.id@greenpeace.org
+                        Supporter Care Officer kami dengan senang hati membantu Anda.
+                    </p>
+                    <p>
+                        Untuk menghindari pendebitan terjadi lagi, kami menganjurkan Anda
+                        untuk mengajukan pembatalan kepada kami 30 hari sebelum
+                        tanggal pendebitan di bulan mendatang.
+                    </p>
+                    <p>
+                        Perjuangan lingkungan tidak dapat dilakukan hanya dalam waktu
+                        singkat saja. Kami terus membutuhkan dukungan Anda. Oleh
+                        karenanya, Tim Supporter Care Greenpeace mungkin menyarankan
+                        Anda tetap melanjutkan dukungan Anda dengan jumlah yang lebih
+                        rendah atau mengatur ulang periode donasi. Silakan beri tahu kami
+                        jika Anda merasa nyaman dengan jumlah donasi tertentu yang akan
+                        digunakan untuk membantu kampanye kami saat ini dan yang akan
+                        datang.
+                    </p>
+                </div>
+            </div>
+            <div class="grid-auto-column">
+                <h5>12. Apakah saya dapat mengajukan refund atas donasi yang sudah
+                    terdebit?</h5>
+                <div>
+                    <p>
+                        Greenpeace Indonesia menerapkan kebijakan pengembalian donasi
+                        sebagai bagian dari komitmen kami untuk menghargai dukungan
+                        finansial yang diberikan individu-individu seperti Anda kepada kami.
+                        Kami menyadari pentingnya donasi Anda dan ingin memastikan
+                        bahwa kami menetapkan prinsip keterbukaan sehubungan dengan
+                        proses pengembalian donasi.
+                    </p>
+                    <p>
+                        Greenpeace Indonesia akan berusaha untuk mengembalikan donasi
+                        sesuai dengan ketentuan berikut ini:<br>
+                        * Jika terjadi kesalahan dalam proses autodebet donasi, kami akan
+                        memproses permintaan pengembalian donasi yang dibuat secara
+                        tertulis dalam waktu 15 (lima belas) hari sejak tanggal donasi
+                        diberikan.<br>
+                        * Permintaan tertulis atas pengembalian donasi harus mencakup
+                        informasi seperti berikut <strong>
+                            nama donatur, tanggal donasi, jumlah
+                            donasi, metode donasi (sebutkan nama bank atau kartu kredit yang
+                            digunakan), dan alasan pengembalian dana.</strong><br>
+                        * Semua permintaan tertulis tersebut dapat dikirim melalui email ke:
+                        <span class="text-link">supporterservices.id@greenpeace.org</span>
+                        dengan subyek:
+                        <strong>Refund_Nama_NoTelepon</strong><br>
+                        * Jika kesalahan dilakukan oleh Greenpeace Indonesia atau payment
+                        gateway (portal pembayaran) kami, pengembalian donasi secara
+                        penuh akan dilakukan setelah Greenpeace Indonesia menerima
+                        informasi tentang kesalahan tersebut secara tertulis dari donatur.<br>
+                        * Greenpeace Indonesia tidak berkewajiban untuk memberikan
+                        pengembalian donasi dan keputusan pengembalian donasi akan
+                        menjadi kebijakan organisasi.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
---
+++ tentang-kami.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+        
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <!-- hero section -->
+    <section>
+        <div style="position: relative;">
+            <div style="width: 95%;margin: auto auto;">
+                <img src="images/tentang-kami-hero.png" class="img-cover"  alt="">
+            </div>
+            <div class="f-inter footer-hero-container" style="left: 50%;">
+            
+                <h1 class="footer-title-header">Tentang Kami</h1>
+                <h4 class="footer-title-subheader">Greenpeace hadir karena bumi yang rapuh ini perlu
+                    suara. Butuh solusi. Butuh perubahan. Butuh aksi.</h4>
+                <p>Di seluruh dunia, kita berdiri bersama-sama dengan masyarakat, menuntut pertanggung
+                    jawaban berbagai pemerintahan dan perusahaan untuk bertanggung jawab. Mulai dari
+                    jalanan hingga ke tempat para pengambil keputusan, kita mempunyai kekuatan nyata jika
+                    kita bekerja sama.</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="news-container f-montserrat">
+            <img src="images/tentang-kami-kapal.png" class="img-cover" style="margin-top: 115px;" alt="">
+            <p>
+                12 orang berangkat dari Vancouver, Kanada menggunakan kapal bernama Phyllis Cormack, di dalam foto ini,
+                namanya ditutupi dengan Greenpeace, untuk memprotes uji coba bom nuklir Amerika Serikat di Amchitka,
+                Alaska. Ini adalah sebagian dari mereka sedang berfoto di haluan kapal.
+            </p>
+            <h1 class="f-montserrat" style="margin-top: 1rem;">Nilai Dasar</h1>
+            <p class="mb-4">
+                <strong> Aksi tanpa kekerasan dan bertanggung jawab secara pribadi.</strong> 
+                Kami beraksi dengan
+                penuh kesadaran. Artinya kami siap bertanggung jawab untuk setiap aksi yang
+                dilakukan, termasuk bertanggung jawab secara pribadi. Kami berkomitmen penuh
+                terhadap perdamaian; semua aktivis Greenpeace yang mengikuti aksi telah mendapat
+                pelatihan tentang aksi damai.
+            </p>
+            <p class="mb-4">
+                <strong> Independensi.</strong> Sejak berdiri, Greenpeace tidak pernah menerima dana dari
+                pemerintah, perusahaan atau partai politik manapun. Donasi individu adalah tulang
+                punggung kampanye Greenpeace. <span style="color: #006DFD;">Baca laporan audit tahunan di sini</span>.
+            </p>
+            <p class="mb-4">
+                <strong> Greenpeace tidak punya lawan atau kawan yang permanen. </strong> Jika sebuah pemerintahan
+                atau perusahaan punya komitmen untuk membuat perubahan positif, kami akan
+                bekerja sama demi mewujudkan perubahan itu. Tetapi jika komitmen itu berubah atau
+                tidak dijalankan, kami akan kembali berkampanye. Yang terpenting adalah aksi nyata,
+                bukan kata-kata.
+            </p>
+            <p class="mb-4">
+                <strong> Mempromosikan solusi.</strong>  Melakukan tekanan tidaklah cukup; kami merancang,
+                melakukan riset dan mengkampanyekan langkah-langkah nyata demi terwujudnya
+                masa depan yang hijau dan damai bagi kita semua.
+            </p>
+            <h1>Struktur Kepengurusan Greenpeace Indonesia</h1>
+            <h3 class="mb-4">Rapat-Rapat</h3>
+            <p class="mb-4">
+                <strong>Rapat Anggota:</strong> Merupakan otoritas tertinggi di dalam Perkumpulan Greenpeace
+                Indonesia.
+            </p>
+            <p class="mb-4">
+                <strong>Rapat Pengurus:</strong> Merupakan rapat yang dapat diadakan setiap waktu bila dipandang
+                perlu atas permintaan tertulis dari satu orang atau lebih Pengurus
+            </p>
+            <p class="mb-4">
+                <strong>Rapat Pengawas</strong> Merupakan rapat yang dapat diadakan setiap waktu bila dianggap
+                perlu atas permintaan tertulis dari seorang atau lebih Pengawas. 
+            </p>
+            <h3>Dewan Pengawas</h3>
+            <h5>Arimbi Heroepoetri — Ketua</h5>
+            <h5>Ir. Abdon Nababan — Sekretaris</h5>
+            <h5>Yosep Adi Prasetyo — Bendahara</h5>
+            <h5 class="mb-4">Harry Surjadi — Anggota</h5>
+            <h1 class="mb-4">Kepala (Country Director)</h1>
+            <p class="mb-4">Dipilih dan ditetapkan serta bertanggung jawab kepada Badan Pengurus dalam
+                pelaksanaan program-program Greenpeace Indonesia dan bertindak mewakili
+                Perkumpulan dalam urusan-urusan hukum, baik di dalam maupun di luar Pengadilan.</p>
+            <p class="mb-4">Adapun susunan pelaksana program Greenpeace adalah sebagai berikut:</p>
+            <p><strong>Leonard Simanjuntak</strong> — Country Director</p>
+            <p><strong>Kiki Taufik</strong> — Global Head of Indonesia Forest campaign</p>
+            <p><strong>Danang Prasetyo</strong> — Deputy Director Engagement</p>
+            <p><strong>Didit Haryo</strong> — Climate & Energy Campaign Team Leader</p>
+            <p><strong>Arifsyah M Nasution</strong> — Senior Regional Campaign Strategist (Ocean Biodiversity)r</p>
+            <p><strong>Afdillah</strong> — Oceans Campaign Team Leader</p>
+            <p><strong>Muharram Atha Rasyadi</strong> — Urban Issue Campaigner</p>
+            <p><strong>Afif Saputra</strong> — Digital & Communication Manager</p>
+            <p><strong>Khalisah Khalid</strong> — Public Engagement & Actions Manager</p>
+            <p><strong>Leonard Simanjuntak</strong> — Country Director</p>
+            <p><strong>Sofyan Siradj</strong> — Human Resources Manager</p>
+            <p><strong>Pendy Salim</strong> — Finance & Admin Manager</p>
+
+            <div class="grid-auto-column" style="font-family: Inter; padding: 1rem;">
+                <div class="card-tentang-kami       " style="max-width:300px;">
+                    <img src="images/sejarah.png" alt="">
+                    <div class="card-title">Sejarah Greenpeace
+                    </div>
+                    <div class="news-slug">
+                        Di tahun 1971, dengan motivasi menjadikan dunia menjadi hijau dan damai, sekelompok aktivis
+                        berlayar dari Vancouver, Cananda, untuk melakukan protes terhadap rencana uji coba bom nuklir
+                        Amerika Serikat di pulau Amchitka, pesisir barat Alasaka.
+                    </div>
+                    <div class="card-footer">
+                       <a href="">Ketahui Sejarah Greenpeace ></a>
+                    </div>
+                </div>
+                <div class="card-tentang-kami" style="max-width:300px;">
+                    <img src="images/Kemenangan.png" alt="">
+                    <div class="card-title">Kemenangan
+                    </div>
+                    <div class="news-slug">Di tahun 1971, dengan motivasi menjadikan dunia menjadi hijau dan damai,
+                        sekelompok aktivis berlayar dari Vancouver, Cananda, untuk melakukan protes terhadap rencana uji
+                        coba bom nuklir Amerika Serikat di pulau Amchitka, pesisir barat Alasaka.</div>
+                        <div class="card-footer">
+                            Kemenangan Yang Kita Raih >
+                        </div>  
+                </div>
+                <div class="card-tentang-kami" style="max-width:300px;">
+                    <img src="images/Pendanaan.png" alt="">
+                    <div class="card-title">Pendanaan
+                    </div>
+                    <div class="news-slug">Di tahun 1971, dengan motivasi menjadikan dunia menjadi hijau dan damai,
+                        sekelompok aktivis berlayar dari Vancouver, Cananda, untuk melakukan protes terhadap rencana uji
+                        coba bom nuklir Amerika Serikat di pulau Amchitka, pesisir barat Alasaka.</div>
+                        <div class="card-footer">
+                           Kontak >
+                        </div>
+                </div>
+                <div class="card-tentang-kami" style="max-width:300px;">
+                    <img src="images/faqs.png" alt="">
+                    <div class="card-title">FAQ
+                    </div>
+                    <div class="news-slug">Jika kamu mesih ingin
+                        mengetahui lebih lanjut tentang
+                        Greenpeace bisa melihat
+                        pertanyaan-pertanyaan umum
+                        yang sering kami terima, semoga
+                        kamu bisa menemukan jawaban
+                        yang kamu cari di sini. Namun
+                        jika masih belum silahkan
+                        menghubungi kami di nomor
+                        kontak yang tertera di laman
+                        FAQ.</div>
+                        <div class="card-footer">
+                            FAQ >
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
