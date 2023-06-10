4d41918bd368e55d00a9f0a9b9989521fc470d8a
nadya <nadndyaa@gmail.com>
2023-06-10 13:34:55 +0700

    ketentuan

---
+++ ketentuan.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <!-- hero section -->
+
+    <div class="f-inter container-kebijakan-privasi"
+        style="background-image: url(images/ketentuan.png);background-repeat: no-repeat;padding: 130px 130px;background-size: 100% 1000px;font-size: 20px;">
+        <h6 style="color: #006DFD;">#TentangKami</h6>
+        <h1 class="footer-title-header">Ketentuan</h1>
+        <h4 class="footer-title-subheader mb-4">Kepemilikan editorial atas konten di situs web</h4>
+
+        <p>
+            Greenpeace adalah sebuah jaringan yang terdiri dari Greenpeace International dan 27
+            organisasi Greenpeace nasional dan regional (NRO), yang semuanya merupakan entitas yang
+            terpisah dan independen.
+        <p>Konten pada setiap subbagian dari greenpeace.org diterbitkan dan dikelola oleh entitas
+            yang relevan. Halaman yang alamatnya dimulai dengan
+            <a href="http://www.greenpeace.org/indonesia" class="text-link">http://www.greenpeace.org/indonesia</a> ,
+            misalnya, berada di bawah kendali editorial
+            Greenpeace Indonesia. Untuk pertanyaan, komentar, atau umpan balik lainnya, silakan
+            hubungi <a href="" class="text-link">entitas Greenpeace yang bertanggung jawab.</a>  Anda umumnya dapat melihat di
+            “URL” atau di bagian atas setiap halaman situs web entitas mana Anda berada.
+        </p>
+        <p>
+            Demi kenyamanan, situs web ini dan publikasi lainnya terkadang hanya merujuk kata
+            “Greenpeace” ketika berbicara tentang satu atau lebih organisasi dalam jaringan. Demikian
+            pula, kata “kita”, “kami” dan “milik kita” juga dapat digunakan untuk merujuk entitas lain
+            dalam jaringan Greenpeace terkait staf, aktivis, dan relawan. NRO kadang-kadang disebut
+            sebagai “kantor” (meskipun satu NRO dapat memiliki lebih dari satu kantor fisik).
+        </p>
+        </p>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
