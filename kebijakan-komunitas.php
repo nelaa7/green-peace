55765b7cbcf033ac0c491a4e64cef50813862e13
nadya <nadndyaa@gmail.com>
2023-06-10 13:43:47 +0700

    k k up

---
+++ kebijakan-komunitas.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <!-- hero section -->
+
+    <div class="f-inter container-kebijakan-privasi"
+        style="background-image: url(images/kebijakan-komunitas.png);background-repeat: no-repeat;padding: 130px 130px;background-size: 100% 1200px;font-size: 20px;">
+        <h1 class="footer-title-header">Kebijakan Komunitas</h1>
+        <h4 class="footer-title-subheader">Jadilah bagian dari komunitas Greenpeace</h4>
+        <p>
+            Di Greenpeace, kami bertekad untuk mewujudkan dunia yang lebih hijau dan damai, dan
+            cara untuk mewujudkannya adalah dengan komunikasi yang terbuka dan jujur.
+        </p>
+        <p>
+            Kami percaya bahwa setiap orang berhak mengutarakan pendapat dengan harapan dapat
+            menemukan kesepakatan bersama. Kami mengedepankan kompromi yang memungkinkan
+            kita untuk maju bersama-sama menuju masa depan yang lebih cerah.
+        </p>
+        <p>
+            Inilah sebabnya bagian komentar pada setiap cerita kami merupakan ruang aman untuk
+            kita semua dapat berbincang dan bertukar pikiran satu sama lain, bahkan dengan mereka
+            yang mempunyai visi berbeda.
+        </p>
+        <p>
+            Kami menyambut hangat diskusi dan debat karena kami yakin dari sanalah ide-ide terbaik
+            muncul. Kami sangat senang melihat banyak orang bersemangat untuk membela apa yang
+            mereka yakini, juga mendapat kesempatan untuk mendengar ide atau melihat suatu
+            permasalahan dari sudut pandang lain. Namun, kami ingin menegaskan bahwa diskusi
+            yang terjadi harus berdasarkan kebenaran dan saling menghormati pendapat lain.
+        </p>
+        <p>
+            Kami meminta semua orang yang ingin berkontribusi dalam percakapan yang terjadi di
+            situs ini, termasuk semua yang membuat pos atas nama Greenpeace, untuk mengikuti
+            aturan berikut:
+        </p>
+        <div class="px-3">
+            <h3 class="mb-4">Katakan kebenaran</h3>
+            <p>
+                <strong>Jujur mengenai siapa dirimu</strong> dan kenapa kamu ada di situs ini. Jika kamu mewakili
+                kepentingan perusahaan atau pemerintah, tidak apa jika ingin berkontribusi dalam
+                percakapan yang terjadi. Namun, kami akan sangat menghargai jika kamu terbuka agar
+                orang lain dapat mengerti dan menghargai pendapatmu.
+            </p>
+            <p>
+                <strong>Sopan dan tenggang rasa</strong>dalam setiap kata-kata yang kamu tulis. Tulislah hal-hal
+                seperti yang ingin kamu baca. Jangan menulis komentar diskriminatif, menyerang
+                pribadi, menggunakan bahasa kasar, menulis pernyataan fitnah, dan menghasut
+                kekerasan.
+            </p>
+            <p>
+                <strong>Beri kesempatan agar semua orang dapat berpartisipasi </strong> – kami dapat menghapus pos
+                jika kamu membuat pos secara berlebihan.
+            </p>
+            <p>
+                <strong>Tetap fokus kepada topik yang diperbincangkan</strong> karena ini akan membantu orang lain
+                yang ingin berpartisipasi dalam percakapan.
+            </p>
+            <p><strong>
+                    Tidak berjualan, </strong> ini adalah ruang untuk berdebat mengenai permasalahan penting, jadi
+                mohon untuk tidak melakukan segala aktivitas komersial atau promosi massal karena
+                kami mempunyai nol toleransi untuk spam. </p>
+        </div>
+        <p>
+            Jika kamu mengikuti aturan di atas, kami tidak akan menghapus komentarmu hanya
+            karena kami tidak setuju dengan apa yang kamu katakan.
+        </p>
+        <p>
+            Kami percaya bahwa kebebasan berbicara dan pertukaran pendapat adalah hal yang sangat
+            penting, jadi kami berusaha sebisa mungkin untuk mempertahankannya. Artinya, jika kamu
+            hanya ingin membuat kerusuhan atau mempunyai niat yang tidak baik, kami akan
+            mengambil langkah untuk melindungi semangat komunitas ini. Langkah-langkah ini dapat
+            termasuk menghapus komentarmu (kami tidak akan mengubahnya), menutup utas atau
+            bahkan mencekal akun. Kami juga dapat mematikan seksi komentar untuk beberapa pos,
+            atau situs, jika kami menemukan serangan bersama sedang dilakukan.
+        </p>
+        <p>
+            Kami menghimbau jika ada anggota komunitas ini yang menemukan aktivitas
+            mencurigakan seperti di atas, mohon dilaporkan ke Webmaster
+             kami.
+        </p>
+        <p>
+            Kami berusaha untuk memastikan bahwa semua hal yang tertera di situs kami adalah
+            akurat dan sejalan dengan nilai yang diperjuangkan Greenpeace. Namun, konten dan
+            komentar  yang ditinggalkan oleh anggota komunitas merupakan tulisan orang biasa
+            sepertimu. Kami harap kamu mengerti bahwa apa yang mereka lakukan belum tentu
+            merepresentasikan pandangan Greenpeace Indonesia atau organisasi Greenpeace di
+            negara lain. Untuk pernyataan resmi dari Greenpeace Indonesia, silakan membaca siaran
+            pers kami, publikasi, atau hubungi Press Desk
+            kami.
+        </p>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
