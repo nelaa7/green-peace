53f9c1f285d74ed1a692557886abf615105cfb10
nadya <nadndyaa@gmail.com>
2023-06-10 13:34:18 +0700

    kebijakan p

---
+++ kebijakan-privasi.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <!-- hero section -->
+
+    <div class="f-inter container-kebijakan-privasi"
+        style="background-image: url(images/kebijakan-privasi.png);background-repeat: no-repeat;padding: 130px 130px;background-size: 100% 1000px;font-size: 20px;">
+        <h6 style="color: #006DFD;">#TentangKami</h6>
+        <h1 class="footer-title-header">Kebijakan Privasi</h1>
+        <p>
+            Greenpeace percaya Anda memiliki hak dasar untuk perlindungan privasi di web, dan kami bekerja untuk
+            melindungi keamanan Anda secara online.
+        </p>
+        <p>
+            Greenpeace Indonesia memproses data pribadi yang mengunjungi website kami. Data
+            pribadi akan diproses demi kepentingan pemberian informasi mengenai kegiatan-kegiatan
+            organisasi Greenpeace, memenuhi ketentuan dan kebutuhan penggalangan dana.
+            Greenpeace Indonesia juga akan menggunakan data pribadi untuk menginformasikan
+            mengenai perkembangan terbaru, kegiatan-kegiatan atau untuk meminta partisipasi anda
+            dalam petisi online atau berbagi pesan.
+        </p>
+        <p>
+            Harap diingat: Jika Anda memberikan informasi kepada Greenpeace Indonesia sebagai
+            bagian dari partisipasi petisi online atau menyampaikan pesan kepad pemerintah,
+            perusahaan, organisasi atau kepada individu, informasi  tersebut akan Greenpeace
+            Indonesia sampaikan kepada yang bersangkutan, sehingga Anda mungkin akan menerima
+            balasan dari sang penerima.
+        </p>
+        <p>
+            Jika Anda mendaftar kepada salah satu mailing list atau Greenpeace Indonesia, kami akan
+            memberikan informasi terbaru mengenai aksi-aksi yang Anda lakukan di website ini,
+            beserta informasi-informasi lainnya. Jika Anda ingin berhenti berlangganan, sangat mudah,
+            Anda tinggal meng-klik “unsubscribe” link yang terletak di bagian bawah setiap email yang
+            kami kirimkan.
+        </p>
+        <p>
+            Greenpeace Indonesia tidak menjual atau menyewakan milis website atau informasi
+            pribadi lainnya kepada pihak-pihak di luar organisasi Greenpeace. Greenpeace Indonesia
+            membagi daftar hanya antar kantor Greenpeace nasional maupun regional  yang
+            memerlukan informasi tersebut demi kampanye yang dilakukan serta keperluan
+            penggalangan dana.  Meskipun cara tercepat dan ramah lingkungan dalam menyebarkan
+            informasi adalah melalui e-mail, namun alamat e-mail Anda hanya akan digunakan oleh
+            kantor Greenpeace nasional maupun Regional apabila telah memperoleh persetujuan
+            Anda. Bahkan jika Anda telah menyetujuinya, kapan pun Anda bisa berhenti berlangganan,
+            dengan meng-klik “unsubscribe” yang selalu tersedia di setiap e-mail.
+        </p>
+        <p>
+            Greenpeace Indonesia menyimpan beberapa catatan individu yang menghubungi kami
+            untuk informasi lebih lanjut atau untuk meminta informasi, maka Greenpeace Indonesia
+            perlu menghubungi mereka atau memberikan informasi lebih lanjut untuk mereka di masa
+            depan. Namun, Greenpeace Indonesia tidak akan memberikan data pribadi yang
+            bersangkutan kepada pihak lain, kecuali disetujui oleh yang bersangkutan. Greenpeace
+            Indonesia tidak akan menggunakan informasi tersebut untuk keperluan lainnya.
+        </p>
+        <p>
+            Situs kami dikelola oleh berbagai server. Statistik perangkat lunak kami mengumpulkan
+            informasi tentang tanggal dan waktu di mana situs web kami diakses, dan juga dapat
+            melacak gerakan pengguna situs. Greenpeace Indonesia menggunakan data ini untuk
+            meningkatkan kinerja situs kami dan untuk mengevaluasi statistik lalu lintas, misalnya hari
+            per hari dalam seminggu di mana Greenpeace Indonesia melihat waktu yang sangat sibuk,
+            dan untuk menilai popularitas relatif dari bidang yang berbeda dari situs. Website ini
+            menggunakan Google Analytic, jasa layanan analisis web yang dikelola oleh Google.Inc
+            (“Google”).Google Analytic menggunakan “cookies”. Cookie adalah elemen kecil dari data
+            yang dapat dikirimkan oleh website kami yang akan tersimpan di perangkat keras Anda.
+            Informasi yang dihasilkan oleh cookie tentang penggunaan situs web (termasuk alamat IP
+            Anda) ditransmisikan dan disimpan oleh Google pada server di Amerika Serikat.Google
+            menggunakan informasi ini untuk melacak bagaimana Anda menggunakan situs web,
+            menyusun laporan kegiatan website untuk operator website dan memberikan layanan lain
+            yang berkaitan dengan kegiatan website dan penggunaan internet.. Google juga dapat
+            mentransfer informasi ini kepada pihak ketiga yang sah, atau di mana pihak ketiga tersebut
+            memproses informasi atas nama Google.Google dapat menggabungkan alamat IP Anda
+            dengan data lain yang dimiliki oleh Google. Dengan menggunakan website ini, Anda setuju
+            atas pengolahan data oleh Google dengan cara dan untuk tujuan yang tercantum di atas.
+        </p>
+        <p>
+            Situs web kami juga menggunakan cookies untuk menyesuaikan pilihan tertentu dan
+            memudahkan pengguna terdaftar untuk meninggalkan komentar dan mengisi formulir
+            online tanpa memasukkan data kembali, dan untuk meningkatkan pengalaman
+            pengunjung.
+        </p>
+        <p>
+            Anda dapat mengatur browser web Anda untuk memberitahu Anda bila Anda menerima
+            cookie, atau menonaktifkannya sama sekali. Dengan demikian, situs web mungkin tidak
+            berfungsi secara optimal. Silahkan melihat informasi tertentu melalui link berikut untuk
+            browser yang paling banyak digunakan di situs Greenpeace Indonesia.
+        </p>
+        <div class="indent-3">
+            <p>Cookies and Internet Explorer please see: </p>
+            <p class="text-link">http://support.microsoft.com/gp/cookies/en</p>
+            <p>
+                Cookie and Firefox, please see: <span class="text-link">
+                    http://support.mozilla.com/en-US/kb/Cookies</span>
+            </p>
+            <p>
+                Cookie and Google Chrome, please see:
+            </p>
+            <p class="text-link">
+                http://www.google.com/support/chrome/bin/answer.py?hl=en&answer=95647
+            </p>
+            <p>
+                Cookie and Safari, please see:</p>
+            <p class="text-link">http://docs.info.apple.com/article.html?
+                path=Safari/3.0/en/9277.html</p>
+            <p>Cookie and Opera, please see:</p>
+            <p class="text-link">
+                http://www.opera.com/browser/tutorials/security/privacy/</p>
+        </div>
+        <p>Greenpeace Indonesia menerapkan langkah-langkah teknis dan pegorganisasian yang
+            tepat untuk mengamankan data pribadi terhadap kehilangan atau terhadap setiap bentuk
+            penggunaan yang melanggar hukum. Halaman donasi kami sangat aman karena untuk itu
+            kami menggunakan Secure Sockets Layer (SSL) untuk mengamankan informasi transaksi
+            kartu kredit Anda, sebuah protokol keamanan yang memberikan privasi komunikasi di
+            Internet dengan cara yang dirancang untuk mencegah eavesdropping, kerusakan, atau
+            pemalsuan pesan. Sedangkan pada secure page, seperti  pada formulir, browser Anda akan
+            mengingatkan Anda bahwa Anda berada di situs yang aman.</p>
+        <p>
+            Greenpeace Indonesia menggunakan SSL enkript untuk melindungi informasi online yang
+            sensitif, Greenpeace Indonesia juga melindungi penggunaan informasi off-line. Semua
+            informasi Anda, tidak hanya informasi sensitif seperti nomor kartu kredit yang terlindungi.
+            Hanya Karyawan yang memang membutuhkan informasi tersebut untuk pekerjaan
+            tertentu (seperti, tim supporter care) yang diberikan ijin mengakses data pribadi tersebut.
+        </p>
+        <p>
+            Jika Anda memiliki pertanyaan mengenai bagaimana kami memproses data pribadi Anda,
+            atau mengenai data pribadi apa saja yang diproses, silahkan menghubungi:
+            <a href="" class="text-link"> supporterservices.id@greenpeace.org</a>
+            . Greenpeace Indonesia wajib meminta bukti
+            identitas sebelum memberikan informasi yang ditanyakan.
+        </p>
+        <p>
+            Dengan bergabung sebagai donor, Anda menerima kebijakan privasi ini dan memberikan
+            ijin kepada Greenpeace untuk mengumpulkan, menyimpan dan memproses sesuai
+            peruntukkan yang tercantum diatas dan telah Anda ketahui dan sepakati.
+        </p>
+        <p>
+            Kebijakan ini akan ditinjau secara teratur. Jika Anda memiliki pertanyaan mengenai
+            kebijakan ini, bisa dialamatkan ke:
+        </p>
+        <h3 class="mb-4">Greenpeace SEA Indonesia :</h3>
+        <p>Mega Plaza Building Lt. 5, Jl. HR. Rasuna Said Kav. C3
+            Kuningan, Jakarta Selatan, Indonesia 12920</p>
+        <p>Tel : +62 21 521 2552</p>
+        <p> Fax: +62 21 521 2553</p>
+        <p>Email : info.id@greenpeace.org</p>
+        <p>Supporter email : supporterservices.id@greenpeace.org</p>
+        <h1 class="mb-4">Jenis cookie</h1>
+        <p>Ada dua jenis cookie yang luas – ‘cookie pihak pertama’ dan ‘cookie pihak ketiga’:</p>
+        <p>Cookie pihak pertama adalah cookie yang disajikan langsung oleh operator situs web ke
+            komputer Anda, dan sering digunakan untuk mengenali komputer Anda ketika
+            mengunjungi kembali situs itu dan mengingat preferensi Anda saat Anda menjelajahi situs.
+            Pada dasarnya, ini adalah cookie kami.
+            Cookie pihak ketiga dilayani oleh penyedia layanan atas nama operator situs web, dan
+            dapat digunakan oleh penyedia layanan untuk mengenali komputer Anda ketika Anda
+            mengunjungi situs web lain. Cookie pihak ketiga paling umum digunakan untuk analisis
+            situs web atau tujuan iklan.</p>
+        <p>Penggunaan cookie
+            Tabel ini menjelaskan cookie yang mungkin ditempatkan pada mesin Anda saat
+            menggunakan situs web ini dan alasannya.</p>
+        <p>Greenpeace Indonesia tidak mengatur publikasi cookie pihak ketiga. Silakan kunjungi situs
+            web pihak ketiga yang relevan jika Anda ingin memahami lebih banyak tentang cookie ini,
+            seperti Facebook, Twitter, Instagram, YouTube atau Soundcloud.</p>
+        <p>Jika Anda tidak ingin menggunakan cookie, Anda dapat mengatur browser Anda sehingga
+            penyimpanan cookie tidak diterima.</p>
+        <p>Jika Anda hanya ingin menerima cookie kami sendiri tetapi bukan cookie dari penyedia
+            dan mitra layanan kami, pilih “blokir cookie pihak ketiga” (atau pengaturan serupa di
+            browser Anda).</p>
+        <h1 class="mb-4">Ubah preferensi cookie Anda</h1>
+        <p>Silakan pilih cookie mana yang ingin Anda simpan.</p>
+        <p>Cookie pengalaman pengguna
+            Cookie ini akan memberi Anda pengalaman yang lebih baik dari situs web kami. Anda akan
+            dapat menyembunyikan spanduk penerimaan cookie dan menggunakan fitur situs web
+            dengan lebih baik. Tidak menerima cookie ini akan memberi Anda pengalaman bebas
+            cookie.</p>
+        <h3>Kinerja Cookie</h3>
+        <p>Cookies ini membantu meningkatkan kinerja Greenpeace.org. Mereka diatur untuk
+            mengumpulkan data seperti berapa lama pengguna bertahan di suatu halaman atau tautan
+            mana yang diklik. Ini membantu kami membuat konten yang lebih baik berdasarkan
+            pengalaman Anda menavigasi situs web.</p>
+        <p>Sebagian besar browser web memungkinkan kontrol atas cookie melalui pengaturan
+            browser (mis. Pemberitahuan cookie baru, menonaktifkan cookie, dan menghapus cookie).
+            Klik jenis browser Anda di bawah ini untuk langsung ke panduan pengguna browser untuk
+            mempelajari cara menonaktifkan atau menghapus cookie.</p>
+        <p>
+            Chrome<br>
+            Firefox <br>
+            Internet Explorer<br>
+            Microsoft Edge<br>
+            Opera<br>
+            Safari<br>
+            Namun, memblokir semua cookie akan berdampak negatif pada kegunaan banyak situs
+            web.
+        </p>
+        <h3>Penggunaan Google Analytics</h3>
+        <p>Greenpeace menggunakan Google Analytics untuk mengumpulkan pengunjung
+            menggunakan situs web kami dan menganalisis data untuk membantu kami
+            mempertahankan dan meningkatkan desain situs web kami. Google Analytics
+            menghasilkan informasi statistik dan lainnya tentang penggunaan situs web dengan
+            cookie, yang disimpan di komputer pengguna.</p>
+        <p>Jika Anda tidak ingin mendapatkan cookie analitik, harap ubah pengaturan cookie Anda di
+            atas (‘ubah preferensi cookie Anda’).</p>
+        <p>Seperti yang disebutkan sebelumnya, cookie Google Analytics yang digunakan dalam situs
+            web ini adalah _utma, _utmb, _utmc, _utmz, _ga dan _gid – dan mereka semua
+            mengumpulkan informasi dalam bentuk anonim, termasuk jumlah pengunjung ke situs,
+            browser ClientID (untuk mengidentifikasi pengguna yang kembali , misalnya), dari mana
+            pengunjung datang ke situs dari dan halaman yang mereka kunjungi. Di sini adalah ikhtisar
+            privasi Google Analytics, Penggunaan Cookie Google Analytics di Situs Web dan di sini
+            cara mudah memilih keluar dari cookie Google Analytics.</p>
+        <p>Pada Mei 2018, Google Analytics memperkenalkan kontrol Penyimpanan Data, memberi
+            organisasi kemampuan untuk mengatur jumlah waktu sebelum data tingkat pengguna dan
+            tingkat peristiwa yang disimpan oleh Google Analytics dihapus secara otomatis dari server
+            Analytics.</p>
+        <p>
+            Periode penyimpanan tersebut berlaku untuk data tingkat pengguna dan tingkat peristiwa
+            yang terkait dengan cookie, pengidentifikasi pengguna (mis. ID Pengguna) dan
+            pengidentifikasi iklan (toh tidak berlaku di situs web Greenpeace). Untuk Greenpeace
+            Internasional, kontrol Retensi Data untuk Google Analytics diatur ke 26 bulan. Sekali lagi,
+            Analytics tidak akan mengumpulkan alamat protokol internet (IP) yang digunakan untuk
+            menghubungkan perangkat Anda ke internet.
+        </p>
+        <p>
+            Selain itu, penting untuk menyebutkan bahwa Google menyimpan informasi lalu lintas
+            situs web yang memiliki kode pelacakan Google Analytics. Periksa kebijakan privasi Google
+            dan cara memilih keluar dari cookie Google Analytics.
+        </p>
+        <p>
+            Anda dapat menginstal “Pengaya Peramban Penyisih Google Analytics” untuk mencegah
+            informasi tentang kunjungan Anda ke situs web kami dikirim ke Google Analytics.
+        </p>
+        <h3>Penggunaan tombol dan widget berbagi jaringan sosial</h3>
+        <p>Selain tombol HTML khusus yang kami pasang untuk memungkinkan Anda berbagi konten
+            Greenpeace di Facebook dan Twitter, kami dapat menyematkan widget dari Facebook,
+            Twitter, Instagram, YouTube, Soundcloud dan media sosial lainnya di situs web kami untuk
+            memberikan pengalaman yang lebih baik.
+        </p>
+        <p>
+            Widget jejaring sosial ini berasal dari server web yang dapat melacak perilaku Anda di situs
+            web kami dan dapat menggabungkan ini dengan informasi lain tentang Anda yang berasal
+            dari situs web lain. Anda dapat menginstal add-on browser seperti “Privacy Badger” untuk
+            Chrome atau “Disconnect” untuk Chrome dan Firefox untuk mengontrol beberapa (yang
+            pertama) atau menonaktifkannya (yang kedua).
+        </p>
+        <h3>Hubungi kami</h3>
+        <p>
+
+            Situs web ini dimiliki dan dioperasikan oleh Greenpeace Indonesia. Jika Anda memiliki
+            pertanyaan tentang cookie kami atau kebijakan cookie ini, mohon hubungi webmaster.
+        </p>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
