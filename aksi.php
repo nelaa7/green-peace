e814f63130b108004d5aa3b4136d542c78f07b53
nadya <nadndyaa@gmail.com>
2023-06-10 10:13:44 +0700

    aksi

---
+++ aksi.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <!-- hero section -->
+
+    <div class="f-inter hero-aksi"
+        style="background-image: url(images/hero-aksi.png);background-repeat: no-repeat;padding: 50px 100px;background-size: 100% 700px;">
+        <h1 style="width: 50%;">Peranmu Untuk
+            Melindungi Bumi</h1>
+        <h6>Greenpeace percaya bahwa dukungan masyarakat adalah kunci dari keberhasilan Kampanye</h6>
+        <p>Greenpeace yakin bahwa perlindungan lingkungan hidup butuh disuarakan lebih lantang. Kami berkomitmen untuk
+            melindungi iklim, laut, udara, dan hutan di negeri ini. Tapi, kita tidak bisa melakukannya sendiri, kami
+            butuh suaramu, solusimu serta aksimu untuk membuat perubahan ke arah yang lebih baik.</p>
+        <a class="hero-items-button mb-4" href="" style="color: white;font-weight: medium;padding: 10px;">Bergabung
+            Sekarang</a>
+        <h1>Dukung Kampanye Greenpeace</h1>
+        <div class="grid-auto-column mb-4">
+            <div class="card" style="font-family: Montserrat;">
+                <img class="card-image " src="images/aksi-kampanye1.png" alt="Card Image">
+                <div class="card-content">
+                    <h4 class="card-subtitle">Budaya</h4>
+                    <h3 class="card-title">Kekayaan Papua</h3>
+                    <p class="card-description">Seperti diketahui Papua memiliki banyak sekali budaya yang patut diakui.
+                    </p>
+                </div>
+            </div>
+            <div class="card" style="font-family: Montserrat;">
+                <img class="card-image " src="images/aksi-kampanye2.png" alt="Card Image">
+                <div class="card-content">
+                    <h4 class="card-subtitle">Lingkungan</h4>
+                    <h3 class="card-title">Mari Jaga Kebersihan</h3>
+                    <p class="card-description">Saat ini kesadaran tentang kebersihan lingkungan mulai menurun. </p>
+                </div>
+            </div>
+            <div class="card" style="font-family: Montserrat;">
+                <img class="card-image " src="images/aksi-kampanye3.png" alt="Card Image">
+                <div class="card-content">
+                    <h4 class="card-subtitle">Iklim</h4>
+                    <h3 class="card-title">Saya Bersama Hutan Papua</h3>
+                    <p class="card-description">Saat ini Hutan Papua beserta isinya sedang dalam
+                        ancaman yang besar. </p>
+                </div>
+            </div>
+        </div>
+        <div class="grid-auto-column mb-4">
+            <div class="card" style="font-family: Montserrat;">
+                <img class="card-image " src="images/aksi-kampanye4.png" alt="Card Image">
+                <div class="card-content">
+                    <h4 class="card-subtitle">Tambang</h4>
+                    <h3 class="card-title">Tambang Semakin Merajalela </h3>
+                    <p class="card-description">Mari kita dukung Aksi pemberhentian tambang di Indonesia. </p>
+                </div>
+            </div>
+            <a href="aksi-cegah-api.php">
+                <div class="card" style="font-family: Montserrat;">
+                    <img class="card-image " src="images/aksi-kampanye5.png" alt="Card Image">
+                    <div class="card-content">
+                        <h4 class="card-subtitle">Hutan</h4>
+                        <h3 class="card-title">Tim Cegah Api: Stop Kebakaran Hutan</h3>
+                        <p class="card-description">Kebakaran hutan tidak hanya mengancam kehidupan
+                            manusia, tapi juga mengancam satwa liar asli Indonesia yang terancam punah.</p>
+                    </div>
+                </div>
+            </a>
+            <div class="card" style="font-family: Montserrat;">
+                <img class="card-image " src="images/aksi-kampanye6.png" alt="Card Image">
+                <div class="card-content">
+                    <h4 class="card-subtitle">Satwa</h4>
+                    <h3 class="card-title">Orang Utan Endemik Indonesia</h3>
+                    <p class="card-description">Saat ini mari kita dukung pelestarian Orang Utan di Indonesia agar tidak
+                        punah. </p>
+                </div>
+            </div>
+        </div>
+        <h1>Ikut Terlibat Bersama Greenpeace</h1>
+        <p>Ada banyak cara ikut terlibat dalam kampanye lingkungan bersama Greenpeace. Membagikan kampanye kami lewat
+            media sosial, membuat kampanyemu sendiri, menghubungi kantor Greenpeace di negara kamu untuk mencari
+            kesempatan menjadi relawan ataupun bekerja di Greenpeace atau menjadi awak kapal. Bumi kita yang rentan ini
+            membutuhkan solusimu dan juga aksimu.</p>
+        <div class="mb-4" style="display:flex;column-gap:20px ;">
+            <div class="card" style="font-family: Montserrat;">
+                <img class="card-image " src="images/aksi-kampanye7.png" alt="Card Image">
+                <div class="card-content">
+                    <h4 class="card-subtitle">TentangKami</h4>
+                    <h3 class="card-title">Berdonasi </h3>
+                    <p class="card-description">Kamu dapat membela lingkungan sepanjang hidupmu.
+                        Atau bahkan lebih lama dari itu. Berdonasilah hari ini. </p>
+                </div>
+                <div class="card-footer" style="display: flex;justify-content: end;padding-right: 10px">
+                    <a class="hero-items-button mb-4" href=""
+                        style="color: white;font-weight: medium;padding: 10px;width: fit-content;">Ikut Beraksi</a>
+                </div>
+            </div>
+            <div class="card" style="font-family: Montserrat;">
+                <img class="card-image " src="images/aksi-kampanye8.png" alt="Card Image">
+                <div class="card-content">
+                    <h4 class="card-subtitle">Aktivisme</h4>
+                    <h3 class="card-title">Bergabung Menjadi Relawan</h3>
+                    <p class="card-description">Bumi kita membutuhkan suara dan aksimu untuk
+                        mencari solusi dan membuat perubahan menjadi nyata,
+                        mari bergerak bersama Greenpeace Indonesia.</p>
+                </div>
+                <div class="card-footer" style="display: flex;justify-content: end;padding-right: 10px;">
+                    <a class="hero-items-button mb-4" href=""
+                        style="color: white;font-weight: medium;padding: 10px;width: fit-content;">Ikut Beraksi</a>
+                </div>
+
+            </div>
+
+        </div>
+        <h1>Menjadi Aktivitas Digital</h1>
+        <p>Perkembangan zaman semakin dinamis, teknologi informasi salah satu yang paling cepat berubah. Saat ini untuk
+            menyuaraan isu-isu perlindungan lingkungan tidak hanya melalui kegiatan konvensional saja. Jadilah bagian
+            dari aktivis digital untuk mengatakan isu perlindungan lingkungan. Apa sih yang bisa kita lakukan di dunia
+        </p>
+        <div class="grid-auto-column" style="font-family: Inter; padding: 1rem;">
+            <div class="" style="max-width:300px;">
+                <img src="images/1.png" class="mb-4" alt="">
+                <div class="card-title">Ikuti Sosial Media
+                </div>
+                <div class="news-slug">Ikuti sosial media Greenpeace Indonesia
+                    untuk mendapatkan informasi terbaru
+                    terkait kampanye perlindungan
+                    lingkungan yang Greenpeace lakukan,
+                    dan jangan lupa tap dua kali di post
+                    instagram, like status kami di facebook,
+                    retweet cuitan kami, dan subcribe
+                    channel youtube kami.</div>
+            </div>
+            <div class="" style="max-width:300px;">
+                <img src="images/2.png" class="mb-4" alt="">
+                <div class="card-title">Pelajari Isunya
+                </div>
+                <div class="news-slug">Karena pengetahuan adalah kunci dan
+                    melindungi Bumi dengan itu kita semua
+                    bisa untuk melihat apa saja akar
+                    masalahnya dan siapa saja yang
+                    bertanggung jawab atasnya. Karena itu
+                    mari bersama pelajari isunya, supaya kita
+                    dapat melindungi Bumi ini dengan lebih
+                    tepat dan efektif.</div>
+            </div>
+            <div class="" style="max-width:300px;">
+                <img src="images/3.png" class="mb-4" alt="">
+                <div class="card-title">Bagikan Kampanyenya
+                </div>
+                <div class="news-slug">Kamu mungkin tahu apa yang terjadi
+                    dengan lingkungan Bumi kita, tapi jangan
+                    simpan pengetahuan ini sendirian,
+                    bagikan kepada teman, pacar, hingga
+                    keluargamu, supaya mereka juga tahu
+                    dan ikut ambil bagian untuk
+                    menyelamatkan Bumi kita. Lagipula, kita
+                    semua tinggal di Bumi yang sama kan.</div>
+            </div>
+            <div class="" style="max-width:300px;">
+                <img src="images/4.png" class="mb-4" alt="">
+                <div class="card-title">Ciptakan Aksimu
+                </div>
+                <div class="news-slug">Mulai dari melayangkan surat kepada
+                    perusahaan, menanam pohon,
+                    melakukan efisiensi energi atau
+                    mengingatkan orang-orang dengan cara
+                    yang kreatif melalui sosial mediamu,
+                    adalah salah satu dari banyak aksi yang
+                    bisa kamu lakukan untuk ikut ambil
+                    bagian dalam menyelamatkan Bumi kita.</div>
+            </div>
+        </div>
+
+    </div>
+    <section>
+        <div class="news-container">
+            <div class="news-header">Artikel Terbaru</div>
+
+            <div class="news-item">
+                <div>
+                    <img src="images/news3.png" style="width: 100%; height: 100%;object-fit: cover;" alt="">
+                </div>
+                <div>
+                    <div class="news-tag"><a href="">Cerita
+                            •
+                            #
+                            Aktivisme
+                            #
+                            Demokrasi
+                            #
+                            Hutan
+                            #
+                            Perdamaian
+                            #
+                            Pekerjaan</a></div>
+                    <div class="news-tittle"><a href="">Gerakan Lingkungan dan Buruh Mesti Bersatu, Sebab Perjuangan
+                            Iklim adalah
+                            Perjuangan Kelas</a></div>
+                    <div class="news-slug">Di Indonesia dan pelbagai belahan dunia, kelas pekerja terlibat dalam
+                        perjuangan lingkungan
+                        hidup dan keadilan iklim.</div>
+                    <span class="news-author">Arie Rompas</span>
+                    <span class="news-date-released">1 Mei 2023</span>
+                </div>
+            </div>
+            <div class="news-item">
+                <div>
+                    <img src="images/news4.png" style="width: 100%; height: 100%;object-fit: cover;" alt="">
+                </div>
+                <div>
+                    <div class="news-tag"><a href="">Cerita
+                            •
+                            #
+                            Aktivisme
+                            #
+                            Demokrasi
+                            #
+                            Hutan
+                            #
+                            Perdamaian
+                            #
+                            Pekerjaan</a></div>
+                    <div class="news-tittle"><a href="">Gerakan Lingkungan dan Buruh Mesti Bersatu, Sebab Perjuangan
+                            Iklim adalah
+                            Perjuangan Kelas</a></div>
+                    <div class="news-slug">Di Indonesia dan pelbagai belahan dunia, kelas pekerja terlibat dalam
+                        perjuangan lingkungan
+                        hidup dan keadilan iklim.</div>
+                    <span class="news-author">Arie Rompas</span>
+                    <span class="news-date-released">1 Mei 2023</span>
+                </div>
+            </div>
+            <div class="news-item">
+                <div>
+                    <img src="images/related2.png" style="width: 100%; height: 100%;object-fit: cover;" alt="">
+                </div>
+                <div>
+                    <div class="news-tag"><a href="">Cerita
+                            •
+                            #
+                            Pekerjaan</a></div>
+                    <div class="news-tittle"><a href="">Dicari: Direct Dialogue Senior Team Leader</a></div>
+                    <div class="news-slug">Memimpin dan mengelola tim Juru Kampanye Dialog Langsung (DDC) yang sedang
+                        berkembang, melatih dan mendukung mereka, dengan berkonsultasi dengan Direct Dialogue
+                        Coordinator dalam meningkatkan marjin pendapatan yang terus meningkat bagi organisasi
+                        berdasarkan target bulanan yang ditentukan.</div>
+                    <span class="news-author">Naviri Chandra</span>
+                    <span class="news-date-released">28 April 2023</span>
+                </div>
+            </div>
+            <a href="artikel-lebaran.php">
+                <div class="news-item">
+                    <div>
+                        <img src="images/news1.png" style="width: 100%; height: 100%;object-fit: cover;" alt="">
+                    </div>
+                    <div>
+                        <div class="news-tag"><a href="">Cerita • #iklim</a></div>
+                        <div class="news-tittle"><a href="">Lebaran di Desa yang Tenggelam</a></div>
+                        <div class="news-slug">Merayakan Idulfitri di desa yang terdampak krisis iklim.</div>
+                        <span class="news-author">Greenpeace Indonesia</span>
+                        <span class="news-date-released">23 April 2023</span>
+                    </div>
+                </div>
+            </a>
+
+            <!-- <div class="news-button">
+                Baca Lebih Lanjut
+            </div> -->
+        </div>
+    </section>
+    <section>
+        <div class="news-container" style="font-family: Inter;">
+            <div class="news-header">Bersama kita bisa membuat perubahan.</div>
+            <div class="news-header" style="font-size: 20px;color:#636363;">Bergabung sekarang untuk mendapatkan
+                informasi terbaru seputar kampanye lingkungan.</div>
+        </div>
+    </section>
+    <section>
+        <div style="background-color: #D4F1B8; font-family: Inter;">
+            <div class="news-container" style="padding-bottom: 40px; margin-bottom: 0px;">
+                <div class="grid-auto-column" style="padding-top: 30px;">
+                    <div>
+                        <div>Email</div>
+                        <input class="nav-search-input"
+                            style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="email"
+                            name="" id="">
+                    </div>
+                    <div>
+                        <div>Nama Depan</div>
+                        <input class="nav-search-input"
+                            style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;"
+                            type="nama_depan" name="" id="">
+                    </div>
+                    <div>
+                        <div>Nama Belakang</div>
+                        <input class="nav-search-input"
+                            style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;"
+                            type="nama_belakang" name="" id="">
+                    </div>
+
+                </div>
+                <div class="news-slug">
+                    Greenpeace Southeast Asia memerlukan informasi kontak yang Anda berikan kepada kami untuk
+                    menghubungi Anda tentang produk dan layanan kami. Anda dapat berhenti berlangganan dari komunikasi
+                    ini kapan pun. Untuk informasi tentang cara berhenti berlangganan,serta praktik privasi dan komitmen
+                    kami untuk melindungi privasi Anda, tinjau Kebijakan Privasi kami.
+                </div>
+                <div class="news-button" style="font-weight: bold;">BERGABUNG SEKARANG</div>
+            </div>
+        </div>
+    </section>
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
