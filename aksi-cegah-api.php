1a870d27d67bcf25aa50d8d2c27b71ea6ea5556c
nadya <nadndyaa@gmail.com>
2023-06-10 13:12:23 +0700

    aksi cegah api

---
+++ aksi-cegah-api.php
@@ -0,0 +1,179 @@
+<html>
+
+<head>
+    <link href="./css/style.css" rel="stylesheet" />
+    <link href="./css/navbar.css" rel="stylesheet" />
+    <link href="./css/footer.css" rel="stylesheet" />
+    <title>Greenpeace</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <div class="hero-aksi-cegah-api">
+    </div>
+    <div class="news-container f-inter">
+        <div class="container-aksi-cegah-api">
+            <h6>Lindungi
+                •
+                #
+                Hutan</h6>
+            <h1>Kembalinya Tim Cegah Api
+                untuk Hutan Indonesia</h1>
+
+
+            <div style="margin-bottom: 10px;">
+                <svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.9">
+                        <rect y="0.589844" width="48" height="48" rx="24" fill="#F6F4EF" />
+                    </g>
+                </svg>
+                <svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.9">
+                        <rect y="0.589844" width="48" height="48" rx="24" fill="#F6F4EF" />
+                    </g>
+                </svg>
+                <svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.9">
+                        <rect y="0.589844" width="48" height="48" rx="24" fill="#F6F4EF" />
+                    </g>
+                </svg>
+                <svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.9">
+                        <rect y="0.589844" width="48" height="48" rx="24" fill="#F6F4EF" />
+                    </g>
+                </svg>
+
+            </div>
+            <p>Tim Cegah Api kembali untuk terjun langsung menangani kebakaran hutan yang tidak
+                berkesudahan. Selain mengancam kehidupan manusia melalui polusi asapnya yang
+                berdampak pada kesehatan kita, karhutla juga mengancam keberlangsungan hidup satwa
+                liar asli Indonesia yang terancam punah.</p>
+            <h1 class="mb-4">Selamatkan Hutan Indonesia dari Kebakaran Berkepanjangan</h1>
+            <p>Tahun 2016, Tim Cegah Api dibentuk sebagai wujud dukungan Greenpeace untuk membebaskan hutan Indonesia
+                dari situasi krisis kebakaran hutan dan lahan yang selalu
+                terjadi setiap tahun. Puncaknya pada 2015 silam yang merupakan bencana asap terburuk yang dialami oleh
+                Indonesia dikarenakan peningkatan yang masif akibat karhutla
+                (bnpb.go.id).</p>
+            <p>Terdiri dari relawan berbagai daerah, Tim Cegah Api berjuang 100% untuk hutan Indonesia. Mereka bekerja
+                sama dengan masyarakat di areal terdampak untuk melakukan
+                pemetaan, pemantauan titik api, dan mengawasi bila ada pengeringan kanal gambut baru, mengenalkan teknik
+                pencegahan kebakaran di pemukiman, sekolah, lingkungan
+                konsesi, juga institusi pemerintahan, dan banyak kegiatan lainnya.</p>
+            <p>Tahun ini, setelah 2 tahun vakum akibat Pandemi COVID-19, Tim Cegah Api kembali untuk menyelamatkan hutan
+                Indonesia tepatnya di Kubu Raya, Kalimantan Barat. Terdiri
+                dari 19 relawan yang berasal dari Riau, Kalimantan, dan Jawa Timur, Tim Cegah Api mengawali kegiatan
+                dengan pelatihan teknis dan non teknis sebelum bertugas langsung ke
+                lapangan.</p>
+            <div class="mb-4" style="width: 100%;height: 100%;">
+                <img src="images/aksi-cegah-api1.png" class="img-cover" alt="">
+            </div>
+            <h1 class="mb-4">Pelatihan Tim Cegah Api </h1>
+            <p>Sebelum melakukan kegiatan pencegahan dan pemadaman api, Tim Cegah Api selalu dibekali dengan pelatihan
+                khusus yang dibutuhkan untuk bertugas di lapangan. Tahun ini,
+                Tim Cegah Api kembali dibekali dengan pelatihan seperti pemetaan, monitoring titik api, penggunaan
+                peralatan pemadam kebakaran hutan, investigasi potensi kebakaran,
+                edukasi pencegahan kebakaran, hingga pelatihan diskusi dan negosiasi dengan perusahaan dan pemerintah.
+                Pelatihan ini dilaksanakan pada 29 Agustus hingga 5 September
+                2022 di Pontianak sebelum Tim Cegah Api bertugas di lapangan selama 30 hari. Mereka bersama-sama
+                mengasah kemampuan dalam hal pemadaman kebakaran hutan agar
+                semakin tangguh dan tanggap dalam beraksi mencegah api yang melahap hutan Indonesia.</p>
+            <div class="grid-auto-column mb-4">
+                <div><img src="images/aksi-cegah-api2.png" class="img-cover" alt=""></div>
+                <div><img src="images/aksi-cegah-api3.png" class="img-cover" alt=""></div>
+                <div><img src="images/aksi-cegah-api4.png" class="img-cover" alt=""></div>
+                <div><img src="images/aksi-cegah-api5.png" class="img-cover" alt=""></div>
+            </div>
+            <h1 class="mb-4">Aktivitas Tim Cegah Api</h1>
+            <p>Hingga Juli 2022, status siaga darurat bencana asap akibat karhutla ditetapkan di 5 (lima) provinsi yaitu
+                Riau, Sumatera Selatan, Jambi, Kalimantan Selatan, dan Kalimantan
+                Barat dimana 131 peristiwa kebakaran hutan dan lahan terjadi. Tim Cegah Api kembali beraksi di daerah
+                Kalimantan Barat, di mana titik api semakin hari kian bertambah, area
+                terbakar semakin meluas dan dampak yang ditimbulkan seperti kabut asap yang semakin pekat dan polutan
+                yang dihasilkan sudah membahayakan kesehatan masyarakat
+                sekitar.</p>
+
+            <div class="grid-auto-column">
+                <div class="" style="">
+                    <div class="card-title">Bersama Masyarakat
+                    </div>
+                    <div class="news-slug" style="height: 200px;">Bekerja sama dengan masyarakat di wilayah
+                        Kalimantan Barat, Tim Cegah Api akan kembali
+                        beraksi untuk berupaya mencegah api datang lagi di
+                        hutan dan lahan gambut kita, sekaligus membantu
+                        mengedukasi masyarakat untuk meningkatkan
+                        kesadaran dan pemahaman tentang dampak
+                        kebakaran hutan bagi kemanusiaan,
+                        keanekaragaman hayati, dan iklim global.</div>
+                    <div class="card-footer">
+                        <button class="btn-aksi-cegah-api">Dukung</button>
+                    </div>
+                </div>
+                <div class="" style="">
+                    <div class="card-title">Wujudkan Hutan Tanpa Api
+                    </div>
+                    <div class="news-slug" style="height: 200px;">Kamu bisa ambil bagian dalam misi penyelamatan
+                        hutan Indonesia dengan berdonasi sekarang!
+                        Donasi yang kamu berikan akan digunakan untuk
+                        melakukan investigasi, dokumentasi, dan lobi
+                        perlindungan hutan yang termasuk di dalamnya
+                        adalah membiayai aktivitas Tim Cegah Api dalam
+                        mencegah terjadinya kebakaran hutan di Indonesia.</div>
+                    <div class="card-footer">
+                        <button class="btn-aksi-cegah-api">Donasi Sekarang</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section>
+        <div class="news-container" style="font-family: Inter;">
+            <div class="news-header">Bersama kita bisa membuat perubahan.</div>
+            <div class="news-header" style="font-size: 20px;color:#636363;">Bergabung sekarang untuk mendapatkan informasi terbaru seputar kampanye lingkungan.</div>
+        </div>
+    </section>
+    <section>
+        <div style="background-color: #D4F1B8; font-family: Inter;">
+            <div class="news-container" style="padding-bottom: 40px; margin-bottom: 0px;">
+                <div class="grid-auto-column" style="padding-top: 30px;">
+                    <div>
+                        <div>Email</div>
+                        <input class="nav-search-input"
+                            style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;" type="email"
+                            name="" id="">
+                    </div>
+                    <div>
+                        <div>Nama Depan</div>
+                        <input class="nav-search-input"
+                            style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;"
+                            type="nama_depan" name="" id="">
+                    </div>
+                    <div>
+                        <div>Nama Belakang</div>
+                        <input class="nav-search-input"
+                            style="border-radius: 0;margin:10px 0px 10px 0px;padding: 0px 10px 0px 10px;"
+                            type="nama_belakang" name="" id="">
+                    </div>
+
+                </div>
+                <div class="news-slug">
+                    Greenpeace Southeast Asia memerlukan informasi kontak yang Anda berikan kepada kami untuk
+                    menghubungi Anda tentang produk dan layanan kami. Anda dapat berhenti berlangganan dari
+                    komunikasi
+                    ini kapan pun. Untuk informasi tentang cara berhenti berlangganan,serta praktik privasi dan
+                    komitmen
+                    kami untuk melindungi privasi Anda, tinjau Kebijakan Privasi kami.
+                </div>
+                <div class="news-button" style="font-weight: bold;">BERGABUNG SEKARANG</div>
+            </div>
+        </div>
+    </section>
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
